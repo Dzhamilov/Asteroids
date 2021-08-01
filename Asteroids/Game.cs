fb36c097a37d0d6890b5bfee91d5daef5bbbc374
Dzhamilov <akbarshoh-95@mail.ru>
2021-08-01 17:43:02 +0300

    инициализация движения объектов (камень и звёзды) астероида

--- Asteroids/Game.cs
+++ Asteroids/Game.cs
@@ -13,6 +13,9 @@ namespace Asteroids
         private static BufferedGraphicsContext _context;
         public static BufferedGraphics Buffer;
 
+        static Asteroid[] _asteroids;
+        static Asteroid[] _stars;
+
 
         public static int Width { get; set; }
         public static int Height { get; set; }
@@ -25,13 +28,60 @@ namespace Asteroids
             Height = form.ClientSize.Height;
             Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
 
+            Load();
+
+            Timer timer = new Timer();
+            timer.Interval = 100;
+            timer.Tick += OnTime;
+            timer.Start();
+
+        }
+
+        private static void OnTime(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
         }
 
         public static void Draw()
         {
             Buffer.Graphics.Clear(Color.Black);
             Buffer.Graphics.FillEllipse(Brushes.LightGoldenrodYellow, new Rectangle(500, 70, 150, 150));
+
+            foreach (Asteroid asteroid in _asteroids)
+                asteroid.Draw();
+
+            foreach (Asteroid star in _stars)
+                star.Draw();
+
             Buffer.Render();
+        }
+
+        public static void Load()
+        {
+            var random = new Random();
+
+            _asteroids = new Asteroid[20];
+            for (int i = 0; i < _asteroids.Length; i++)
+            {
+                var size = random.Next(5, 20);
+                _asteroids[i] = new Asteroid(new Point(0, i * 10 + 5), new Point(-i - 2, -i - 2), new Size(size, size));
+            }
+
+            _stars = new Asteroid[20];
+            for (int i = 0; i < _stars.Length; i++)
+            {
+                _stars[i] = new Star(new Point(600, i * 25 + 15), new Point(-i - 3, -i - 3), new Size(4, 3));
+            }
+        }
+
+        public static void Update()
+        {
+            foreach (Asteroid asteroid in _asteroids)
+                asteroid.Update();
+
+            foreach (Asteroid star in _stars)
+                star.Update();
         }
 
     }
