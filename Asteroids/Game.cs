64925af152f10eaa95528e18098a5997a4676723
Dzhamilov <akbarshoh-95@mail.ru>
2021-08-01 16:43:17 +0300

    Добавьте файлы проекта.

---
+++ Asteroids/Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asteroids
+{
+    static class Game
+    {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;
+
+
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+
+        public static void Init(Form form)
+        {
+            Graphics g = form.CreateGraphics();
+            _context = BufferedGraphicsManager.Current;
+            Width = form.ClientSize.Width;
+            Height = form.ClientSize.Height;
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+
+        }
+
+        public static void Draw()
+        {
+            Buffer.Graphics.Clear(Color.Black);
+            Buffer.Graphics.FillEllipse(Brushes.LightGoldenrodYellow, new Rectangle(500, 70, 150, 150));
+            Buffer.Render();
+        }
+
+    }
+}
